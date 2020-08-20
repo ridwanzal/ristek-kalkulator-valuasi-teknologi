b7e81dd180ffb207d3e7f3bb122408fbbb08a9a4
root <zalbinaridwan@gmail.com>
2020-08-20 13:32:11 +0700

    login and styles

--- application/views/login.php
+++ application/views/login.php
@@ -11,8 +11,8 @@
                             <span class="input-group-addon nk-ic-st-pro"><i class="notika-icon notika-support"></i></span>
                             <div class="form-group float-lb">
                                 <div class="nk-int-st">
-                                    <input type="text" class="form-control" placeholder="Username" name="p_username" required>
-                                    <label class="nk-label">Username</label>
+                                    <input type="text" class="form-control" placeholder="Please provide E-mail Address" name="p_username" required>
+                                    <label class="nk-label">Please provide E-mail Address</label>
                                 </div>
                             </div>
                         </div>
@@ -20,17 +20,30 @@
                             <span class="input-group-addon nk-ic-st-pro"><i class="notika-icon notika-edit"></i></span>
                             <div class="form-group float-lb">
                                 <div class="nk-int-st">
-                                    <input type="password" class="form-control" placeholder="Password" name="p_password" autocomplete="on" required>
-                                    <label class="nk-label">Password</label>
+                                    <input type="password" class="form-control" placeholder="Please provide Password" name="p_password" autocomplete="on" required>
+                                    <label class="nk-label">Please provide Password</label>
                                 </div>
                             </div>
                         </div>
-                        <input style="background:#000; color:#fff;width:100%; border:1px solid #000;padding:10px;border-radius:4px;" type="submit" class="" value="Login">
+                        <input style="background:#10586E; color:#fff;width:100%; border:1px solid #10586E;padding:10px;border-radius:4px;" type="submit" class="" value="Login">
+                    </div>
+                    <div style="margin-top:25px; color:#10586E">
+                        <!-- <p style="color:#fff;">Belum punya akun terdaftar ? </p>&nbsp;&nbsp;<a href="<?php echo base_url()?>register">Register</a> -->
+                        <label class="">Bantuan ?</label>
                     </div>
                 </form>
-            <div style="margin-top:25px;">
-                <!-- <p style="color:#fff;">Belum punya akun terdaftar ? </p>&nbsp;&nbsp;<a href="<?php echo base_url()?>register">Register</a> -->
-                <label class="">Bantuan</label>
-            </div>
+            <div style="margin-top:90px;"> 
+                <div class="row">
+                    <div class="col-md-2">
+                        <img style="width:50px;" src="http://sinta.ristekbrin.go.id/assets/img/ristekbrin_logo-circle.png">
+                    </div>
+                    <div class="col-md-10" style="text-align:left;font-size:10px;color:#888;">
+                        Copyright &copy; 2020
+                        <br/>Kementerian Riset dan Teknologi / Badan Riset dan Inovasi Nasional
+                        <br/><i>(Ministry of Research and Technology / National Agency for Research and Innovation)</i>
+                        <br/>All Rights Reserved.
+                    </div>
+                </div>
+            </div>  
         </div>
     </div>
