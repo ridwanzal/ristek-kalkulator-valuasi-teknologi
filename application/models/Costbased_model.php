91fb617cd79a12db8e09785453292e427fe3027e
ridwanzal <zalbinaridwan@gmail.com>
2020-09-20 06:41:22 +0700

    add cost based model

---
+++ application/models/Costbased_model.php
@@ -0,0 +1,36 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Costbased_model extends CI_Model {
+
+    private $table = "sikav_";
+    public $sinta_id;
+    public $id;
+    
+    public function get_sinta($sinta_id)
+    {
+    }
+
+    public function get_id($id)
+    {
+    }
+
+    // insert data
+    function insert($data)
+    {
+    }
+
+    // update data
+    public function update($hki_id, $data)
+    {       
+       
+    }
+
+    // delete data
+    public function delete($hki_id)
+    {
+    }
+
+}
+
