78a38a3db47328d387a119ea071fb6339d0eca61
ridwanzal <zalbinaridwan@gmail.com>
2020-10-03 17:02:20 +0700

    change colors

--- application/views/frontview/index.php
+++ application/views/frontview/index.php
@@ -2,19 +2,28 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-7 col-md-7 col-xs-7"> 
-                <h2 class="main_font" style="margin-bottom:30px;">Selamat Datang di SIKAV</h2>
+                <h6 class="" style="">Selamat Datang di</h6>
+                <span style="font-size:100px;
+                          font-weight:700;
+                          letter-spacing:-5px;
+                          font-family: 'Montserrat', sans-serif !important;">SIKAV
+                </span>
                 <p style="line-height:30px;">
                     SIKAV merupakan Sistem Informasi Kalkulator Valuasi Teknologi.
                     Valuasi Teknologi terkait Kekayaan Intelektual dan Aset tak berwujud. 
                     Kalkulasikan valuasi teknologi dengan pendekatan & metode berbasis
                     Cost, Income dan Market.
                 </p>
+                <br/>
                 <a href="<?php echo base_url();?>login" class="btn btn-success">
                     Uji Coba kalkulasi
+                </a>&nbsp;
+                <a href="<?php echo base_url();?>login" class="btn btn-warning">
+                    Pelajari Lebih Lanjut
                 </a>
             </div>
             <div class="col-lg-5 col-md-5 col-xs-5"> 
-                <img id="image_banners" height="400px" style="position:relative;top:-70px;left:150px;" src="<?php echo base_url();?>/assets/frontview/img/research.svg">
+                <img id="image_banners" height="400px" style="position:relative;top:0px;left:150px;" src="<?php echo base_url();?>/assets/frontview/img/research.svg">
             </div>
         </div>
     </div>
