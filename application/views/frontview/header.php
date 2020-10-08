55bd3d840fea96d872d9af35192e8f49e01bf652
ridwanzal <zalbinaridwan@gmail.com>
2020-10-08 17:54:01 +0700

    sweetalert remove

--- application/views/frontview/header.php
+++ application/views/frontview/header.php
@@ -33,5 +33,4 @@
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
     integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
     crossorigin=""></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.4.0/bootbox.min.js"></script>
 </head>
